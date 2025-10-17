01ae6359460d912eb0349b43ad16c86c97a6f798
陈枫元 <chenfengyuan@chenfengyuandeMacBook-Pro.local>
2025-10-17 01:19:56 -0400

    all task done

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -24,11 +24,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         // return statement included so that the starter code can compile and run.
         final Response response;
         final JSONObject responseBody;
@@ -46,11 +42,11 @@ public class DogApiBreedFetcher implements BreedFetcher {
                 }
                 return breedList;
             }else {
-                throw new BreedNotFoundException("Breed Not Found");
+                throw new BreedNotFoundException(breed);
             }
         }
         catch(IOException | BreedNotFoundException event){
-            throw new BreedNotFoundException("Breed Not Found");
+            throw new BreedNotFoundException(breed);
         }
     }
 }
